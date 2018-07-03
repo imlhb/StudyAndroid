82ec62926509e3d008114b53363ac5978226b89a
lhb <514660094@qq.com>
2018-07-04 00:37:40 +0800

    创建自定义控件

---
+++ UICustomViews/app/src/main/java/com/example/uicustomviews/TitleLayout.java
@@ -0,0 +1,31 @@
+package com.example.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context , AttributeSet attrs){
+        super(context , attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button title_edit = (Button) findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        title_edit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"你按了编辑",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
