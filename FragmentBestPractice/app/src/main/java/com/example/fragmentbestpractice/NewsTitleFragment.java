f9313a900e5019f80f7f666bdd8f4d6ab793c28c
lhb <514660094@qq.com>
2018-07-11 22:51:23 +0800

    一个简易版的新闻应用

--- FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -6,12 +6,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 //作为展示新闻列表的碎片
 public class NewsTitleFragment extends Fragment {
@@ -25,14 +28,36 @@ public class NewsTitleFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         newsTitleRecyclerView.setLayoutManager(layoutManager);
         NewsAdapter adapter = new NewsAdapter(getNews());
-        newsTitleRecyclerView
+        newsTitleRecyclerView.setAdapter(adapter);
         return view;
     }
+
+    private List<News> getNews(){
+        List<News> newsList = new ArrayList<>();
+        for (int i = 1 ;i <= 50 ;i++){
+            News news = new News();
+            news.setTitle("这是新标题"+i);
+            news.setContent(getRandomLengthContent("这是新内容"+i+"."));
+            newsList.add(news);
+        }
+        return newsList;
+    }
+
+    private String getRandomLengthContent(String content){
+        Random random = new Random();
+        int length = random.nextInt(20)+1;
+        StringBuilder builder = new StringBuilder();
+        for (int i =0 ; i<length;i++){
+            builder.append(content);
+        }
+        return builder.toString();
+    }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (getActivity().findViewById(R.id.news_title_layout) != null){
+        if (getActivity().findViewById(R.id.news_content_layout) != null){
             isTwoPane = true;//可以找到 news_content_layout 布局时，为双页
         }else {
             isTwoPane = false;//找不到 news_content_layout 布局时，为单页
@@ -65,9 +90,12 @@ public class NewsTitleFragment extends Fragment {
                 public void onClick(View view) {
                     News news = mNewsList.get(holder.getAdapterPosition());
                     if (isTwoPane){
+                        Log.d("isTwoPaneTest", String.valueOf(isTwoPane));
+                        //如果是双页模式，则刷新NewsContentFragment中的内容
                         NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager().findFragmentById(R.id.news_content_fragment);
                         newsContentFragment.refresh(news.getTitle(),news.getContent());
                     }else {
+                        //如果是单页模式，则直接启动NewsContentActivity
                         NewsContentActivity.actionStart(getActivity(),news.getTitle(),news.getContent());
                     }
                 }
