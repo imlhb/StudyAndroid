d0f1a8f9e5901c5d7377f52c552f74d7f9eea95f
lhb <514660094@qq.com>
2018-07-16 22:01:29 +0800

    创建自己的内容提供器(完)

---
+++ MyProvider/app/src/main/java/com/example/myprovider/Myprovider.java
@@ -0,0 +1,87 @@
+package com.example.myprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class Myprovider extends ContentProvider{
+
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM = 1;
+    public static final int TABLE2_DIR = 2;
+    public static final int TABLE2_ITEM = 3;
+
+    private static UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.app.provider","table1",TABLE1_DIR);
+        uriMatcher.addURI("com.example.app.provider","table1/#",TABLE1_ITEM);
+        uriMatcher.addURI("com.example.app.provider","table2",TABLE2_DIR);
+        uriMatcher.addURI("com.example.app.provider","table2/#",TABLE2_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        switch (uriMatcher.match(uri)){
+            case TABLE1_DIR:
+                //查询table1表中所有数据
+                break;
+            case TABLE1_ITEM:
+                //查询table1表中单条数据
+                break;
+            case TABLE2_DIR:
+                //查询table2表中所有数据
+                break;
+            case TABLE2_ITEM:
+                //查询table2表中单条数据
+                break;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (uriMatcher.match(uri)){
+            case TABLE1_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table1";
+            case TABLE1_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table1";
+            case TABLE2_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table2";
+            case TABLE2_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table2";
+        }
+        return null;
+    }
+
+}
