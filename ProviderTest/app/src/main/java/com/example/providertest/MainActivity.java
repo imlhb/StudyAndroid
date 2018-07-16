28c12e4573f7ac95ae16c7ccbae3b9abf03a7c2b
lhb <514660094@qq.com>
2018-07-17 02:03:16 +0800

    创建自己的内容提供器（查看端完）

--- ProviderTest/app/src/main/java/com/example/providertest/MainActivity.java
+++ ProviderTest/app/src/main/java/com/example/providertest/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.providertest;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String newId;
+
+    private String TAG = "MainActivityTest";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //添加数据
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name","A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages" , 1203);
+                values.put("price", 22.83);
+                Uri newUri = getContentResolver().insert(uri , values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //查询数据
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book");
+                Log.d(TAG, "onClick: 测试");
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                Log.d(TAG, "onClick: 测试2");
+                if (cursor != null){
+                    while (cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d(TAG, "onClick: name:" + name);
+                        Log.d(TAG, "onClick: author:" + author);
+                        Log.d(TAG, "onClick: pages:" + pages);
+                        Log.d(TAG, "onClick: price:" + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //更新数据
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book/"+ newId);
+                ContentValues values = new ContentValues();
+                values.put("name","A Storm of Swords");
+                values.put("pages",1216);
+                values.put("price" , 24.05);
+                getContentResolver().update(uri ,values ,null,null);
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //删除数据
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book/"+ newId);
+                getContentResolver().delete(uri,null,null);
+            }
+        });
     }
 }
