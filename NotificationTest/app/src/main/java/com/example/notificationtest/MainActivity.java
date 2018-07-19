94a5c4e92ed83251267c44ad0546331735d56449
lhb <514660094@qq.com>
2018-07-19 16:22:06 +0800

    使用通知（完成）

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.notificationtest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.send_notice:
+//意图
+                Intent intent = new Intent(this , NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                //这里原有的NotificationCompat.Builder被弃用，后面括号里面新加入个String参数即可
+                Notification notification = new NotificationCompat.Builder(this,"default")
+                        .setContentTitle("这是标题")
+                        .setContentText("这是内容")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        //意图
+                        .setContentIntent(pi)
+                        //点击后销毁通知
+                        .setAutoCancel(true)
+                        .build();
+                manager.notify(1,notification);
+                break;
+            default:
+                break;
+        }
     }
 }
