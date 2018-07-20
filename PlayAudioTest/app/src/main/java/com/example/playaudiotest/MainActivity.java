f6144373d2970191912925569721071dfef1a6ac
lhb <514660094@qq.com>
2018-07-21 02:08:31 +0800

    播放多媒体文件（新建）(完成)需要在更目录放音乐文件

--- PlayAudioTest/app/src/main/java/com/example/playaudiotest/MainActivity.java
+++ PlayAudioTest/app/src/main/java/com/example/playaudiotest/MainActivity.java
@@ -1,13 +1,106 @@
 package com.example.playaudiotest;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+    private String TAG = "测试测试";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = (Button) findViewById(R.id.play);
+        Button pause = (Button) findViewById(R.id.pause);
+        Button stop = (Button) findViewById(R.id.stop);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else{
+            initMediaPlayer();
+        }
+
+    }
+
+    private void initMediaPlayer(){
+        try{
+
+            File file = new File(Environment.getExternalStorageDirectory(),"Time.mp3");
+            Log.d(TAG, "initMediaPlayer: "+Environment.getExternalStorageDirectory());
+            Log.d(TAG, "initMediaPlayer: "+file);
+            mediaPlayer.setDataSource(file.getPath());//指定音频文件路径
+            Log.d("测试", "initMediaPlayer: "+file.getPath());
+            mediaPlayer.prepare();//让MediaPlayer进入准备状态
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initMediaPlayer();
+                }else {
+                    Toast.makeText(this,"拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.play:
+                if (!mediaPlayer.isPlaying()){
+                    mediaPlayer.start();//开始播放
+                }
+                break;
+            case R.id.pause:
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();//暂停播放
+                }
+                break;
+            case R.id.stop:
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.reset();//停止播放
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 }
